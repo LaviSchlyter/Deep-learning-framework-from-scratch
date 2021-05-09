10d3775b0871d9b0c99ab83296b1ce2b95e47661
schlyter <lavinia.schlyter@epfl.ch>
2021-05-09 12:38:03 +0200

    Change to only use torch.empty write plotting function

--- Project_2/new.py
+++ Project_2/new.py
@@ -3,6 +3,9 @@ from abc import abstractmethod
 
 import torch
 from matplotlib import pyplot as plt
+# TODO How to make this without torch.tensor
+        # TODO Check with Karel assisant message on Slack
+
 
 class Module:
 
@@ -37,8 +40,7 @@ class Sequential:
 
         return par
 
-    def evaluate(self, prediction, labels):
-        pass
+
 
 
 # Tensor
@@ -48,32 +50,46 @@ class Tensor:
 
         assert not isinstance(value, Tensor)
         self.value = value
-        self.grad = torch.zeros_like(value)
+        # self.grad = torch.zeros_like(value)
+        self.grad = Tensor.zeros_like(value)
         self.grad_fn = grad_fn
-
 
     def backward(self, output_grad=None):
 
         if output_grad is None:
-            output_grad = torch.ones_like(self.value)
-
+            # output_grad = torch.ones_like(self.value)
+            output_grad = Tensor.ones_like(self.value)
         if self.grad_fn is not None:
             self.grad_fn.backward(output_grad)
 
         self.grad += output_grad
 
-
     def zero_grad(self):
-        self.grad = torch.zeros_like(self.value)
-
+        #self.grad = torch.zeros_like(self.value)
+        self.grad = Tensor.zeros_like(self.value)
 
     def __getitem__(self, item):
         if isinstance(item, int):
-            item = slice(item,item+1)
-        return Tensor(self.value[:, item], grad_fn= SliceGradFn(self, item))
+            item = slice(item, item + 1)
+        return Tensor(self.value[:, item], grad_fn=SliceGradFn(self, item))
 
     def cat(self, tensor):
-        return Tensor(torch.cat((self.value, tensor.value), 1), grad_fn= CatGradFn(self, tensor))
+
+        size_cat = [self.value.shape[0], self.value.shape[1] + tensor.value.shape[1]]
+        concat = torch.empty(size_cat)
+        concat[:, 0] = self.value[:, 0]
+        concat[:, 1] = tensor.value[:, 0]
+
+        # damn = [self.value, tensor.value]
+
+        # return Tensor(torch.cat((self.value, tensor.value), 1), grad_fn= CatGradFn(self, tensor))
+        return Tensor(concat, grad_fn=CatGradFn(self, tensor))
+
+    def zeros_like(self):
+        return torch.empty(self.shape).zero_()
+
+    def ones_like(self):
+        return Tensor.zeros_like(self) + 1
 
     @property
     def shape(self):
@@ -85,8 +101,10 @@ class Tensor:
 class Linear(Module):
 
     def __init__(self, Din, Dout):
-        self.W = Tensor(torch.rand([Din, Dout]) * (2/math.sqrt(Din)) - 1/math.sqrt(Din))
-        self.b = Tensor(torch.rand(([Dout])))
+        self.W = Tensor(torch.empty([Din, Dout]).uniform_() * (2 / math.sqrt(Din)) - 1 / math.sqrt(Din))
+        # self.W = Tensor(torch.rand([Din, Dout]) * (2/math.sqrt(Din)) - 1/math.sqrt(Din))
+        self.b = Tensor(torch.empty([Dout]).uniform_())
+        # self.b = Tensor(torch.rand(([Dout])))
 
     def __call__(self, input_):
         # Takes input_ of shape NxD and W of DxM
@@ -121,14 +139,17 @@ class Sigmoid(Module):
 
     @staticmethod
     def sigmoid(input_):
-        return 1 / (1 + torch.exp(-input_))
+        # return 1 / (1 + torch.exp(-input_))
+        return 1 / (1 + (-input_).exp())
 
 
 class Relu(Module):
 
     def __call__(self, input_):
         return Tensor(
-            value=torch.maximum(torch.zeros_like(input_.value), input_.value),
+            # value=torch.maximum(torch.zeros_like(input_.value), input_.value),
+            # TODO Fix maximum, am I allowed to do this
+            value=Tensor.zeros_like(input_).maximum(input_.value),
             grad_fn=ReluGradFn(input_)
         )
 
@@ -136,7 +157,8 @@ class Relu(Module):
     @staticmethod
     def forward(input_):
         return Tensor(
-            value=torch.maximum(torch.zeros_like(input_), input_),
+
+            value=Tensor.zeros_like(input_).maximum(input_),
             grad_fn=ReluGradFn(input_)
         )
 
@@ -152,10 +174,10 @@ class SliceGradFn:
         self.item = item
 
     def backward(self, output_grad):
-        input_grad = torch.zeros_like(self.input_.value)
+        # input_grad = torch.zeros_like(self.input_.value)
+        input_grad = Tensor.zeros_like(self.input_.value)
         input_grad[:, self.item] = output_grad
         self.input_.backward(input_grad)
-
 
 
 class CatGradFn:
@@ -164,7 +186,7 @@ class CatGradFn:
         self.input2 = input2
 
     def backward(self, output_grad):
-        input_grad_1 = output_grad[:,:(self.input1.shape[1])]
+        input_grad_1 = output_grad[:, :(self.input1.shape[1])]
         input_grad_2 = output_grad[:, (self.input1.shape[1]):]
         self.input1.backward(input_grad_1)
         self.input2.backward(input_grad_2)
@@ -192,8 +214,7 @@ class ReluGradFn:
     def backward(self, output_grad):
         input_grad = output_grad * (self.input_.value > 0)
         self.input_.backward(input_grad)
-        #self.input_.grad += input_grad
-
+        # self.input_.grad += input_grad
 
 
 class SigmoidGradFn:
@@ -250,14 +271,13 @@ class Adam(Optim):
         m = [0] * len(self.params)
         v = [0] * len(self.params)
 
-
         for i, param in enumerate(self.params):
             # gt = param.grad
             m[i] = self.beta1 * m[i] + (1 - self.beta1) * param.grad
             v[i] = self.beta2 * v[i] + (1 - self.beta2) * param.grad ** 2
             m_hat = m[i] / (1 - self.beta1)
             v_hat = v[i] / (1 - self.beta2)
-            param.value -= self.alpha * m_hat/(torch.sqrt(v_hat) + self.epsilon)
+            param.value -= self.alpha * m_hat / (v_hat.sqrt() + self.epsilon)
 
 
 class SGD(Optim):
@@ -272,7 +292,6 @@ class SGD(Optim):
 
     def step(self):
         for param in self.params:
-
             param.value -= self.lr * param.grad
 
 
@@ -281,17 +300,28 @@ class SGD(Optim):
 class LossMSE:
 
     def __call__(self, input_, target):
-        return Tensor(1 / 2 * ((input_.value - target.value) ** 2).sum(dim = 0), grad_fn=LossMSEGradFN(input_, target))
+        return Tensor(1 / 2 * ((input_.value - target.value) ** 2).sum(dim=0), grad_fn=LossMSEGradFN(input_, target))
 
-    def param(self):
+    @staticmethod
+    def param():
         return []
 
+
 def evaluate(pred, target):
-        return 1 - ( (pred.value > 0.5) == (target.value > 0.5)).sum() / len(pred.value)
+    return 1 - ((pred.value > 0.5) == (target.value > 0.5)).sum() / len(pred.value)
 
-def plot_performance(plot_data, plot_legend):
 
-    pass
+def plot_performance(plot_data, plot_legend, print_loss = True):
+
+    if not print_loss:
+        plot_legend = plot_legend[:2]
+
+    for i, legend in enumerate(plot_legend):
+
+        plt.plot(plot_data[:, i], label=legend)
+    plt.legend()
+    plt.show()
+
 
 class LossMSEGradFN:
     def __init__(self, input_, target):
@@ -303,6 +333,17 @@ class LossMSEGradFN:
         self.input_.backward(input_grad)
         self.target.backward(-input_grad)
 
+# TODO Add cross entropy loss ....
+class LossCrossEntropy:
+
+    def __call__(self, input_, target):
+        return -(input_.value*math.log(target)).sum()
+
+    @staticmethod
+    def param():
+        return []
+
+
 
 
 def generate_disc_set(nb):
@@ -312,13 +353,12 @@ def generate_disc_set(nb):
 
 
 def main():
-
     # Disable the use of autograd from PyTorch
     torch.set_grad_enabled(False)
-    #torch.manual_seed(19)
+    # torch.manual_seed(19)
 
     model = Sequential([Linear(1, 50), Relu(), Linear(50, 25), Relu(), Linear(25, 1), Relu()])
-    model_ = Sequential([Linear(2*1, 50), Relu(), Linear(50, 25), Relu(), Linear(25, 1), Sigmoid()])
+    model_ = Sequential([Linear(2 * 1, 50), Relu(), Linear(50, 25), Relu(), Linear(25, 1), Sigmoid()])
 
     # Generate the set of size n
     n = 1000
@@ -342,13 +382,12 @@ def main():
 
     loss = LossMSE()
     criterion = Adam(model.param() + model_.param())
-    #criterion = SGD(model.param(), 0.05/n)
+    # criterion = SGD(model.param(), 0.05/n)
 
     epoch = 500
-    plot_data = torch.zeros(epoch, 4)
-    plot_legend = "train_error",  "test_error"
+    plot_data = torch.empty(epoch, 4)
+    plot_legend = ["train_error", "test_error", "train_loss", "test_loss"]
     for e in range(epoch):
-
         criterion.zero_grad()
 
         # Train evaluation
@@ -366,33 +405,30 @@ def main():
         y_test_y = model(test_input_y)
 
         y_test = model_(y_test_x.cat(y_test_y))
-        #y_test = model(test_input)
+        # y_test = model(test_input)
         cost_test = loss(y_test, test_target)
         error_test = evaluate(y_test, test_target)
 
         # Save values for data plotting
-        plot_data[e, :] = torch.tensor([error_train, error_test, cost_train.value/n, cost_test.value/n])
+        #plot_data[e,:] = [error_train, error_test, cost_train.value / n, cost_test.value / n]
+
+        plot_data[e, :] = torch.tensor([error_train, error_test, cost_train.value / n, cost_test.value / n])
 
         cost_train.backward()
         criterion.step()
-        #print(f"For epoch = {e} with MSE loss = {cost.value}")
+        # print(f"For epoch = {e} with MSE loss = {cost.value}")
 
 
 
+    plot_performance(plot_data, plot_legend, False)
+    print("test error: ", evaluate(y_test, test_target))
 
-    plt.plot(plot_data[:,0], label = "train_error")
-    plt.plot(plot_data[:,1], label = "test_error")
-    plt.plot(plot_data[:, 2], label="train_cost")
-    plt.plot(plot_data[:, 3], label="test_cost")
-    plt.legend()
+    print("train error: ", evaluate(y_train, train_target))
 
-
-    plt.show()
-    print("test error", evaluate(y_test, test_target))
-
-
-    print("train error: ",evaluate(y_train, train_target))
-
+    test = torch.empty(3, 1)
+    test2 = torch.empty(3, 1)
+    print(test, test2)
+    print(test2.maximum(test))
 
 
 if __name__ == '__main__':
