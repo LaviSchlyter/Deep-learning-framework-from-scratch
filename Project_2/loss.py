6b8fcb37a8243cff43876f17bc696e02966acb4e
schlyter <lavinia.schlyter@epfl.ch>
2021-05-23 19:27:31 +0200

    Changed from sum to mean

--- Project_2/loss.py
+++ Project_2/loss.py
@@ -21,7 +21,6 @@ class LossMSE:
 
     def param(self):
         """
-
         :return: Empty list because loss does not have parameters
         """
         return []
@@ -37,9 +36,10 @@ class LossMSEGradFN:
         Backward method for LossMSEGradFN, where we compute the derivative
         :param output_grad: The gradient w.r.t the output of the layer
         """
-        input_grad = (self.input_.value - self.target.value) * output_grad
+        batch_size = len(self.input_.value)
+
+        input_grad = (self.input_.value - self.target.value) * output_grad/batch_size
         self.input_.backward(input_grad)
-        self.target.backward(-input_grad)
 
 
 class LossBCE:
@@ -56,13 +56,12 @@ class LossBCE:
         assert target.grad_fn is None, "The target HyperCube must not have a grad_fn"
 
         return HyperCube(-(target.value * input_.value.log().clamp(-100, float("inf")) + (1 - target.value) * (
-                1 - input_.value).log().clamp(-100, float("inf"))).sum(dim=0),
+                1 - input_.value).log().clamp(-100, float("inf"))).mean(dim=0),
                          grad_fn=LossBCEGradFN(input_, target))
 
 
     def param(self):
         """
-
         :return: Empty list as loss does not have parameters
         """
         return []
@@ -84,6 +83,7 @@ class LossBCEGradFN:
         x = self.input_.value
         y = self.target.value
         eps = ones_like(x) * 1e-12
-        input_grad = ((x - y) / ((1 - x) * x).maximum(eps)) * output_grad
+        batch_size = len(self.input_.value)
+        input_grad = ((x - y) / ((1 - x) * x).maximum(eps)) * output_grad / batch_size
         self.input_.backward(input_grad)
-        self.target.backward((-x.log() + (1 - x).log()) * output_grad)
+
